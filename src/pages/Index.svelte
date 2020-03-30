3071f49baa10f3f10a9a3eb653f1c59cea541bdf
d0kur0 <dev.dokuro@gmail.com>
2020-03-30 20:31:50 +0800

    Add local navbar rendering

---
+++ src/pages/Index.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Navbar from "/components/Navbar.svelte";
+</script>
+
+<template>
+    <Navbar linkUrl="/settings" linkIcon="settings-outline" videoName="RandomName.webm" />
+</template>
