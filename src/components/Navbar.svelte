fbdd9de2ed1668c98b92c16ba0ed4163500a8a42
d0kur0 <dev.dokuro@gmail.com>
2020-03-30 20:31:29 +0800

    Add input params: linkUrl, linkIcon, videoName

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,15 +1,19 @@
 <script>
 	import { link } from "svelte-spa-router";
+
+	export let linkUrl = "/";
+	export let linkIcon = "";
+	export let videoName = "";
 </script>
 
 <template>
 	<nav class="navbar">
-		<a href="/settings" use:link class="button button--circle">
-			<ion-icon name="settings-outline"></ion-icon>
+		<a href="{linkUrl}" use:link class="button button--circle">
+			<ion-icon name="{linkIcon}"></ion-icon>
 		</a>
 
 		<div class="navbar__video-name">
-			<span class="navbar__video-name-wrapper">name of video.webm</span>
+			<span class="navbar__video-name-wrapper">{videoName}</span>
 		</div>
 	</nav>
 </template>
