bcaf3fd8f517f8168849a6cc217542d53ad6a874
d0kur0 <dev.dokuro@gmail.com>
2020-03-30 20:32:04 +0800

    Add local navbar rendering

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -1,37 +1,37 @@
 <script>
-  const vendors = [
-    {
-      name: "2ch",
-      boards: [
-        {
-          name: "b",
-          description: "Бред"
-        },
-        {
-          name: "a",
-          description: "Аниме"
-        },
-        {
-          name: "h",
-          description: "Хентай"
-        }
-      ]
-    }
-  ];
+  import Navbar from "/components/Navbar.svelte";
+  import { schema } from "/stores/schema.js";
 </script>
 
 <template>
-  <div class="page-wrapper">
-    <div class="checkbox-list">
-      {#each vendors as vendor}
-        <div class="checkbox-list__header">{vendor.name}</div>
+  <Navbar linkUrl="/" linkIcon="arrow-back-outline" />
 
-        {#each vendor.boards as board}
-          <div class="checkbox-list__item">
-            {board.name} ({board.description})
+  <div class="page-wrapper">
+    <h3>Настройка выборки</h3>
+
+    <div class="checkbox-list">
+      {#if !$schema}
+        <div class="loading-bar">
+          <ion-icon name="aperture-outline"></ion-icon>
+        </div>
+      {:else}
+        {#each $schema as vendor}
+          <div class="checkbox-list__header">Вендор: {vendor.name}</div>
+
+          <div class="checkbox-list__items">
+            {#each vendor.boards as board}
+              <label class="checkbox-item">
+                <input class="checkbox-item__input" checked type="checkbox">
+                <span class="checkbox-item__flag"></span>
+
+                <span class="checkbox-item__label">
+                {board.name} ({board.description})
+              </span>
+              </label>
+            {/each}
           </div>
         {/each}
-      {/each}
+      {/if}
     </div>
   </div>
 </template>
