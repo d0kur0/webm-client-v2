0c4a91f1f45eca707ff8eeba018a54eaa13b1a2b
d0kur0 <dev.dokuro@gmail.com>
2020-03-31 17:57:13 +0800

    Add theme toggler

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,19 +1,49 @@
 <script>
 	import { link } from "svelte-spa-router";
+	import { theme } from "/stores/theme.js";
 
 	export let linkUrl = "/";
 	export let linkIcon = "";
 	export let videoName = "";
+	export let themeToggler = true;
+
+	function toggleTheme () {
+		theme.toggleTheme();
+		document.body.classList.add("transition");
+		document.body.classList.toggle("dark", !$theme);
+
+		setTimeout(() => {
+			document.body.classList.remove("transition");
+		}, 500);
+
+		localStorage.overrideTheme = $theme;
+	}
 </script>
 
 <template>
-	<nav class="navbar">
-		<a href="{linkUrl}" use:link class="button button--circle">
-			<ion-icon name="{linkIcon}"></ion-icon>
-		</a>
+	<div class="navbar-top-margin"></div>
 
-		<div class="navbar__video-name">
-			<span class="navbar__video-name-wrapper">{videoName}</span>
+	<nav class="navbar">
+		<div class="navbar__pull-left">
+			<a href="{linkUrl}" use:link class="button button--circle">
+				<ion-icon name="{linkIcon}"></ion-icon>
+			</a>
+
+			<div class="navbar__video-name">
+				<span class="navbar__video-name-wrapper">{videoName}</span>
+			</div>
 		</div>
+
+		{#if themeToggler}
+			<div class="navbar__pull-right">
+				<button class="button button--circle" on:click={toggleTheme}>
+					{#if $theme}
+						<ion-icon name="sunny-outline"></ion-icon>
+					{:else}
+						<ion-icon name="moon-outline"></ion-icon>
+					{/if}
+				</button>
+			</div>
+		{/if}
 	</nav>
 </template>
