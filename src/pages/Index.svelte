1dbd54ad6ac23bb8451a97515e9a40759ee627ab
d0kur0 <dev.dokuro@gmail.com>
2020-04-03 22:50:58 +0800

    Add files fetching

--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -1,5 +1,13 @@
 <script>
     import Navbar from "/components/Navbar.svelte";
+    import { onMount } from "svelte";
+
+    import { schema } from "/stores/schema.js";
+    import { videos } from "/stores/videos.js";
+
+    onMount(async () => {
+    	await videos.fetchBySchema($schema);
+    });
 </script>
 
 <template>
