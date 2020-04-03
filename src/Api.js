0521c0effed2d36ac94139db3c5462e90f71c4b7
d0kur0 <dev.dokuro@gmail.com>
2020-04-03 22:49:31 +0800

    Add fetching files

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,25 @@
 import axios from "axios";
 
+function unpackFiles (vendors) {
+	const files = [];
+
+	for (const vendor in vendors) {
+		if (!vendors.hasOwnProperty(vendor)) continue;
+		const boards = vendors[vendor];
+
+		for (const board in boards) {
+			if (!boards.hasOwnProperty(board)) continue;
+			const { threads } = boards[board];
+
+			threads.forEach(thread => files.push(...thread.files))
+		}
+	}
+
+	console.log(files)
+
+	return files;
+}
+
 function createAPI () {
 	const transport = axios.create({
 		baseURL: "http://5.180.138.37:3500/",
@@ -7,12 +27,13 @@ function createAPI () {
 	});
 
 	return {
-		async getFiles () {
+		async getFilesBySchema (inputSchema) {
+			const schema = { vendors: {} };
+			inputSchema.forEach(vendor => schema.vendors[vendor.name] = vendor.boards.map(board => board.name));
 
-		},
+			const { data } = await transport.post("files/getByStruct", JSON.stringify(schema));
 
-		async getFilesBySchema (schema) {
-
+			return unpackFiles(data.vendors);
 		},
 
 		async getSchema () {
