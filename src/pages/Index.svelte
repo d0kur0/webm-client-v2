e977f5fb5350f71ac5cde480dcd4d869828a1c68
d0kur0 <dev.dokuro@gmail.com>
2020-04-24 00:52:40 +0300

    Add fetching files, add video player component

--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -1,15 +1,27 @@
 <script>
     import Navbar from "/components/Navbar.svelte";
+    import Player from "/components/VideoPlayer/Player.svelte";
+
     import { onMount } from "svelte";
 
     import { schema } from "/stores/schema.js";
     import { videos } from "/stores/videos.js";
 
     onMount(async () => {
-    	await videos.fetchBySchema($schema);
+        await videos.fetchBySchema($schema);
     });
 </script>
 
 <template>
-    <Navbar linkUrl="/settings" linkIcon="settings-outline" videoName="RandomName.webm" />
+  <Navbar linkUrl="/settings" linkIcon="settings-outline" videoName="RandomName.webm" />
+
+  <div class="page-wrapper">
+    {#if $videos.files.length}
+	    <Player />
+  {:else}
+    <div class="loading-bar">
+      <ion-icon name="aperture-outline"></ion-icon>
+    </div>
+  {/if}
+  </div>
 </template>
