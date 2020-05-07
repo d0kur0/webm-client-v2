72c24590c160681c90a28de883b3362ce025cdd4
d0kur0 <dev.dokuro@gmail.com>
2020-05-07 10:00:38 +0300

    Add stores binds

--- src/components/videoPlayer/Player.svelte
+++ src/components/videoPlayer/Player.svelte
@@ -1,7 +1,22 @@
 <script>
+	import Controls from './Controls.svelte';
+	import { videos } from "/stores/videos.js";
+	import { playerCurrentTime } from '/stores/playerCurrentTime.js';
+	import { playerPaused } from "/stores/playerPaused.js";
+	import { playerDuration } from "/stores/playerDuration.js";
 
+	$: currentVideo = $videos.files[$videos.currentIndex];
 </script>
 
 <template>
-	123
+	<div class="player" tabindex="0">
+		<video
+			bind:paused="{$playerPaused}"
+			bind:duration="{$playerDuration}"
+			bind:currentTime={$playerCurrentTime}
+			src="{currentVideo.path}">
+		</video>
+
+		<Controls />
+	</div>
 </template>
