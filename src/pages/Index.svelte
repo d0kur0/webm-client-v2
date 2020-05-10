42f456cc26a51d3b22a3cb9cc692247bf10a421e
d0kur0 <dev.dokuro@gmail.com>
2020-05-10 14:31:24 +0300

    Reident file with tabs, move loader to player component

--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -1,31 +1,24 @@
 <script>
-    import Navbar from "/components/Navbar.svelte";
-    import Player from "/components/VideoPlayer/Player.svelte";
+	import Navbar from "/components/Navbar.svelte";
+	import Player from "/components/VideoPlayer/Player.svelte";
 
-    import { onMount } from "svelte";
+	import { onMount } from "svelte";
 
-    import { schema } from "/stores/schema.js";
-    import { videos } from "/stores/videos.js";
+	import { schema } from "/stores/schema.js";
+	import { videos } from "/stores/videos.js";
 
-    onMount(async () => {
-        await videos.fetchBySchema($schema);
-    });
+	onMount(async () => {
+		await schema.fetch();
+		await videos.fetchBySchema($schema);
+	});
 
-    $: videoName = $videos.files[$videos.currentIndex] 
-                  ? $videos.files[$videos.currentIndex].name 
-                  : "...";
+	$: videoName = $videos.files[$videos.currentIndex] 
+								? $videos.files[$videos.currentIndex].name 
+								: "...";
 </script>
 
 <template>
-  <Navbar linkUrl="/settings" linkIcon="settings-outline" videoName="{videoName}" />
+	<Navbar linkUrl="/settings" linkIcon="settings-outline" videoName="{videoName}" />
 
-  {#if $videos.files.length}
-    <Player />
-  {:else}
-   <div class="page-wrapper">
-      <div class="loading-bar">
-        <ion-icon name="aperture-outline"></ion-icon>
-      </div>
-    </div>
-  {/if}
+	<Player />
 </template>
