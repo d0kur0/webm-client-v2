263a4d266a032fe23eb74ff20e89de835da33dbc
d0kur0 <dev.dokuro@gmail.com>
2020-05-10 14:33:08 +0300

    Reident file with tabs

--- src/stores/playerVolume.js
+++ src/stores/playerVolume.js
@@ -2,7 +2,11 @@ import { writable } from "svelte/store";
 
 // Store factory
 const createStore = () => {
-	const { subscribe, set, update } = writable(0.5);
+	const value = localStorage.volume !== undefined
+							? +localStorage.volume 
+							: 0.5;
+
+	const { subscribe, set, update } = writable(value);
 
 	return {
     set,
