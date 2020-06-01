9ee9ce58fdd7ad5f16236a5b92a4a6af5d9a47e0
d0kur0 <dev.dokuro@gmail.com>
2020-06-01 07:08:18 +0300

    Fix bug: remove recursive checking is mouse cursor on controls for touch devices

--- src/stores/playerUI.js
+++ src/stores/playerUI.js
@@ -1,56 +1,62 @@
 import { writable } from "svelte/store";
 
 const stateStruct = {
-	// HTMLElement of root video container
-	playerElement: undefined,
-	// Flag of fullscreen mode
+  // HTMLElement of root video container
+  playerElement: undefined,
+  // Flag of fullscreen mode
   isFullscreen: false,
 
-	// TimeoutID for controls visible
-	showControlsTimeout: undefined,
-	// Flag of visible controls
-	showControls: true,
+  // TimeoutID for controls visible
+  showControlsTimeout: undefined,
+  // Flag of visible controls
+  showControls: true,
 };
 
 // Store factory
 const createStore = () => {
-	const { subscribe, set, update } = writable(stateStruct, function start (set) {
-		return () => set(stateStruct);
-	});
+  const { subscribe, set, update } = writable(stateStruct, function start(set) {
+    return () => set(stateStruct);
+  });
 
-	return {
-		set,
-		subscribe,
+  return {
+    set,
+    subscribe,
 
-		toggleControls () {
-			update(store => {
-				const timeoutHandler = () => {
+    toggleControls() {
+      update((store) => {
+        const timeoutHandler = () => {
+          const rects = store.playerElement &&
+            store.playerElement.getBoundingClientRect();
+          if (!rects) return;
 
-					const rects = store.playerElement && store.playerElement.getBoundingClientRect();
-					if (!rects) return;
+          const isMouseOnControls = (window.mousePosition.X >= rects.x &&
+            window.mousePosition.X <= rects.x + rects.width) &&
+            (window.mousePosition.Y >= rects.y &&
+              window.mousePosition.Y >= rects.y + rects.height);
 
-					const isMouseOnControls = (window.mousePosition.X >= rects.x && window.mousePosition.X <= rects.x + rects.width) &&
-						(window.mousePosition.Y >= rects.y && window.mousePosition.Y >= rects.y + rects.height);
+          const isTouch = matchMedia(
+            "(hover: none) and (pointer: fine), (hover: none) and (pointer: coarse)",
+          );
 
-					if (isMouseOnControls) {
-						store.showControlsTimeout = setTimeout(timeoutHandler, 2500);
-						return;
-					}
+          if (isMouseOnControls && !isTouch) {
+            store.showControlsTimeout = setTimeout(timeoutHandler, 2500);
+            return;
+          }
 
-					update(function (store) {
-						store.showControls = false;
-						return store;
-					});
-				};
+          update(function (store) {
+            store.showControls = false;
+            return store;
+          });
+        };
 
-				clearTimeout(store.showControlsTimeout);
-				store.showControlsTimeout = setTimeout(timeoutHandler, 2500);
-				store.showControls = true;
+        clearTimeout(store.showControlsTimeout);
+        store.showControlsTimeout = setTimeout(timeoutHandler, 2500);
+        store.showControls = true;
 
-				return store;
-			});
-		}
-	};
+        return store;
+      });
+    },
+  };
 };
 
 export const playerUI = createStore();
